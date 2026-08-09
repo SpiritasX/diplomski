620840996ea8bb3535c483f8d3e3e78152641aa4
Dejan Bogovac <deja.bogovac@gmail.com>
2026-08-09 10:28:14 +0200

    Initial commit

---
+++ frontend/app/routes/api.js
@@ -0,0 +1,7 @@
+// src/api.js
+const API_URL = import.meta.env.VITE_API_URL;
+
+// export async function getUsers() {
+//   const response = await fetch(`${API_URL}/users`);
+//   return response.json();
+// }
